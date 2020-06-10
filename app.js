6ccc32816996448ef2d7f4e9d3c39f145101ceeb
thierry-a <thierryarzal353@gmail.com>
2020-06-10 09:29:32 +0200

    Premier test

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Hello World!" });
+  });
+  
 module.exports = app;
