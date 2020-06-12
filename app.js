2c8c80256c34eb6beb2a1bbfbb7d19b60982e657
thierry-a <thierryarzal353@gmail.com>
2020-06-12 08:41:36 +0200

    challenge - recuperer un bookmark

--- app.js
+++ app.js
@@ -2,12 +2,47 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+require('dotenv').config();
+const connection = require('./connection');
+
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
     res.status(200).json({ message: "Hello World!" });
+});
+
+app.post('/bookmarks', (req, res) => {
+  if (!req.body.url && !req.body.title) {
+    return res.status(422).json({ error: 'required field(s) missing' });
+  }
+  connection.query('INSERT INTO bookmark SET ?', req.body, (err, stats) => {
+    if (err) return res.status(500).json({ error: err.message, sql: err.sql });
+
+    connection.query('SELECT * FROM bookmark WHERE id = ?', stats.insertId,
+      (err, records) => {
+        if (err)
+          return res.status(500).json({ error: err.message, sql: err.sql });
+          return res.status(201).json(records[0]);
+      }
+    );
   });
-  
+});
+
+app.get('/bookmarks/:id', (req, res) => {
+  connection.query('SELECT * FROM bookmark WHERE id = ?', req.params.id, (err, results) => {
+      if (err) {
+          res.status(500).json({error: err.message, sql: err.sql});
+      } else {
+        if(results.length){
+          res.status(200).json(results[0]);
+        } else {
+          res.status(404).json({error: 'bookmark not found'})
+        }
+      }
+  })
+})
+
 module.exports = app;
--- test/app.integration.spec.js
+++ test/app.integration.spec.js
@@ -1,8 +1,12 @@
 // test/app.integration.spec.js
 const request = require('supertest');
 const app = require('../app');
+const connection = require('../connection');
 
 describe('Test routes', () => {
+  // truncate bookmark table before each test
+  beforeEach(done => connection.query('TRUNCATE bookmark', done));
+
   it('GET / sends "Hello World" as json', (done) => {
     request(app)
       .get('/')
@@ -14,4 +18,67 @@ describe('Test routes', () => {
         done();
       });
   });
+
+  it('POST /bookmarks - error (fields missing) ', (done) => {
+    request(app)
+      .post('/bookmarks')
+      .send({})
+      .expect(422)
+      .expect('Content-Type', /json/)
+      .then(response => {
+        const expected = {error: 'required field(s) missing'};
+        expect(response.body).toEqual(expected);
+        done();
+       });
+  });
+
+  it('POST /bookmarks - OK (fields provided) ', (done) => {
+    request(app)
+      .post('/bookmarks')
+      .send({ url: 'https://jestjs.io', title: 'Jest' })
+      .expect(201)
+      .expect('Content-Type', /json/)
+      .then(response => {
+        const expected = { id: expect.any(Number), url: 'https://jestjs.io', title: 'Jest' };
+        expect(response.body).toEqual(expected);
+        done();
+      })
+      .catch(done);
+  });
+
+  describe('GET /bookmarks/:id', () => {
+    const testBookmark = { url: 'https://nodejs.org/', title: 'Node.js' };
+    beforeEach((done) => connection.query(
+      'TRUNCATE bookmark', () => connection.query(
+        'INSERT INTO bookmark SET ?', testBookmark, done
+      )
+    ));
+  
+      it('GET /bookmarks/:id  sends "error - not found', (done) => {
+        request(app)
+          .get('/bookmarks/:id')
+          .expect(404)
+          .expect('Content-Type', /json/)
+          .then(response => {
+            const expected = {error: 'bookmark not found'};
+            expect(response.body).toEqual(expected);
+            done();
+          });
+      });
+  
+      it('GET /bookmarks/:id  sends "success - found', (done) => {
+        request(app)
+          .get('/bookmarks/1')
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .then(response => {
+            const expected = { id: 1, url: 'https://nodejs.org/', title: 'Node.js' };
+            expect(response.body).toEqual(expected)
+          });
+          done();
+      });
+    });  
+
 });
+
+
